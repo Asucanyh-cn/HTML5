7f9e9e62c680cd6ce1b4343069701fc89833545e
asucanyh-cn <asucanyh@outlook.com>
2022-10-17 12:31:56 +0800

    test

--- scripts/asp.js
+++ scripts/asp.js
@@ -1,9 +1,25 @@
-var myImage=document.querySelector('img');
+var myImage=document.querySelector("img");
 myImage.onclick=function(){
-    var src=myImage.getAttribute('src');
-    if(src==='images/firefox-icon.png'){
-        myImage.setAttribute('src','images/gawr_gura.png');
+    var mySrc=myImage.getAttribute('src');
+    if(mySrc=='images/firefox-icon.png'){
+        myImage.setAttribute('src','images/firefox-icon-0.png');
     }else{
         myImage.setAttribute('src','images/firefox-icon.png');
     };
 };
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+  }
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Hello，' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+ }
